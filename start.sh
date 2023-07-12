cc3dd289eb183a7143a134e3d947ced336a9c9fd
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-07-13 00:41:01 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Abdalla2307/Test6677.git /ArrowFlixBot
+  git clone https://github.com/Abdalla2307/Test6677.git /ArrowFlix
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PiroAutoFilterBot
+  git clone $UPSTREAM_REPO /ArrowFlix
 fi
-cd /PiroAutoFilterBot
+cd /ArrowFlix
 pip3 install -U -r requirements.txt
-echo "Starting ArrowFlixBot...."
+echo "Starting ArrowFlix..."
 python3 bot.py
